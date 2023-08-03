a26c12fdd8723d50e648a270eac954fb153814fd
voronyi-artem <110160853+voronyi-artem@users.noreply.github.com>
2023-08-03 14:30:14 +0300

    fix: add link of task

--- 8 kyu/Function1-hello world/main.js
+++ 8 kyu/Function1-hello world/main.js
@@ -1,3 +1,5 @@
+// link https://www.codewars.com/kata/523b4ff7adca849afe000035
+
 // DESCRIPTION:
 // Make a simple function called greet that returns the most-famous "hello world!".
 
@@ -6,6 +8,8 @@
 
 
 // Write a function "greet" that returns "hello world!"
+
+
 let greet = function () {
   return ('hello world!')
 }

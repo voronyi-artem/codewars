dd27b71c083383d0aec9bd5a0cb2ec56f2267e19
voronyi-artem <110160853+voronyi-artem@users.noreply.github.com>
2023-08-07 18:47:50 +0300

    feat: add solution Convert to Binary

---
+++ 8 kyu/Convert to Binary/main.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/59fca81a5712f9fa4700159a
+
+// DESCRIPTION:
+// Task Overview
+// Given a non - negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+// ***************************************************************
+
+function toBinary(n) {
+
+  return Number((n).toString(2));
+
+}

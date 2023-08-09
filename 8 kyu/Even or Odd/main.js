e50d29e4e4cea403e3793fd69df83f6698e2eda1
voronyi-artem <110160853+voronyi-artem@users.noreply.github.com>
2023-08-09 09:19:00 +0300

    feat: add solution Even or Odd

---
+++ 8 kyu/Even or Odd/main.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// *******************************************
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return 'Even'
+  }
+  else {
+    return 'Odd'
+  }
+}

5a4fb41570b60cf283e5ab7d3af53660e3b5d286
voronyi-artem <110160853+voronyi-artem@users.noreply.github.com>
2023-08-15 09:45:26 +0300

    feat: add solution Number of Decimal Digits

---
+++ 8 kyu/Number of Decimal Digits/main.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/users/rsschool_5e0054ed646cb17f/completed_solutions  
+
+
+// DESCRIPTION:
+// Determine the total number of digits in the integer(n >= 0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits.Be careful to avoid overflows / underflows.
+
+// All inputs will be valid.
+
+
+function digits(n) {
+  n = String(n);
+  n = n.length
+  return (n)
+  // code goes here
+}

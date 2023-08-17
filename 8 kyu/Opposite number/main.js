dba64d67a6139f69263303cdf714dabca52e6282
voronyi-artem <110160853+voronyi-artem@users.noreply.github.com>
2023-08-17 09:14:49 +0300

    feat: add solution Opposite number

---
+++ 8 kyu/Opposite number/main.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+// DESCRIPTION:
+// Very simple, given an integer or a floating - point number, find its opposite.
+
+
+function opposite(number) {
+
+  number = (-1 * number)
+
+  return (number)
+
+}
+
